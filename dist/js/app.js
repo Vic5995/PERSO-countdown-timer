8767703727685e41f67c721b8f18cff16b343031
Victoire <victoire.lenglart.5995@gmail.com>
2021-12-22 10:55:10 +0100

    feat: check incorrect date

--- dist/js/app.js
+++ dist/js/app.js
@@ -44,6 +44,11 @@ function countdown() {
     }
 }
 
+function checkDateTimeInput(date, time) {
+    let targetDate = Date.parse(date+"T"+time);
+    return targetDate > Date.now();
+}
+
 // LISTERNERS
 submitBtn.addEventListener('click', ev => {
     ev.preventDefault();
@@ -51,24 +56,29 @@ submitBtn.addEventListener('click', ev => {
     let time = timeInput.value;
     let name = (nameInput.value).toUpperCase();
 
-    let targetDate = Date.parse(date+"T"+time);
-    let start = Date.now();
-    let remain = (targetDate - start) / 1000; // 1 sec = 1000 ms
+    if (checkDateTimeInput(date, time)) {
+        let targetDate = Date.parse(date+"T"+time);
+        let start = Date.now();
+        let remain = (targetDate - start) / 1000; // 1 sec = 1000 ms
 
-    days = Math.floor(remain / 86400); // 1 j = 24 h = 1440 min = 86400 sec
-    hours = Math.floor((remain % 86400) / 3600); // 1 h = 60 min = 3600 sec
-    minutes = Math.floor(((remain % 86400) % 3600) / 60);  // 1 min = 60 sec
-    seconds = Math.floor(((remain % 86400) % 3600) % 60);
+        days = Math.floor(remain / 86400); // 1 j = 24 h = 1440 min = 86400 sec
+        hours = Math.floor((remain % 86400) / 3600); // 1 h = 60 min = 3600 sec
+        minutes = Math.floor(((remain % 86400) % 3600) / 60);  // 1 min = 60 sec
+        seconds = Math.floor(((remain % 86400) % 3600) % 60);
 
-    countdownDisplay.children[0].innerHTML = `COUNTDOWN TO ${name}`
-    countdownDisplay.children[1].innerHTML = days < 10 ? "0" + days : days;
-    countdownDisplay.children[2].innerHTML = hours < 10 ? "0" + hours : hours;
-    countdownDisplay.children[3].innerHTML = minutes < 10 ? "0" + minutes : minutes;
-    countdownDisplay.children[4].innerHTML = seconds < 10 ? "O" + seconds : seconds;
+        countdownDisplay.children[0].innerHTML = name === "" ? "" : `COUNTDOWN TO ${name}`
+        countdownDisplay.children[1].innerHTML = days < 10 ? "0" + days : days;
+        countdownDisplay.children[2].innerHTML = hours < 10 ? "0" + hours : hours;
+        countdownDisplay.children[3].innerHTML = minutes < 10 ? "0" + minutes : minutes;
+        countdownDisplay.children[4].innerHTML = seconds < 10 ? "O" + seconds : seconds;
 
-    countdownIntervalID = setInterval(countdown, 1000);
+        countdownIntervalID = setInterval(countdown, 1000);
 
-    countdownDisplay.classList.remove("hidden");
+        countdownDisplay.classList.remove("hidden");
+    } else {
+        alert("Wrong date and time input");
+        initDisplay();
+    }
 })
 
 startPauseBtn.addEventListener('click', ev => {
@@ -95,7 +105,6 @@ resetBtn.addEventListener('click', ev => {
 // MAIN
 initDisplay()
 
-// TODO warning of incorrect date
 // TODO warning of overflow date ( > 99 days)
 // TODO add an alert when the event is reached
 // TODO responsive design
