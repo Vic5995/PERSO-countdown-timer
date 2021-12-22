276e89caa4c3e4b261602881e8cd4745aaccb645
Victoire <victoire.lenglart.5995@gmail.com>
2021-12-22 10:45:46 +0100

    feat: event name

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,6 +5,7 @@ const submitBtn = document.getElementById('submit-input');
 const startPauseBtn = document.getElementById('pause-restart-btn');
 const resetBtn = document.getElementById('reset-btn');
 const countdownDisplay = document.getElementById('countdown-display');
+const nameInput = document.getElementById('name-input');
 
 // VARIABLES
 let countdownIntervalID;
@@ -16,7 +17,8 @@ function initDisplay() {
     let today = new Date();
     dateInput.value = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
     let minutes = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
-    timeInput.value = `${today.getHours()}:${minutes}`;
+    let hours = today.getHours() < 10 ? "0" + today.getHours() : today.getHours();
+    timeInput.value = `${hours}:${minutes}`;
 }
 
 function countdown() {
@@ -26,18 +28,18 @@ function countdown() {
         running = false;
     } else {
         seconds = --seconds < 0 ? 59 : seconds;
-        countdownDisplay.children[3].innerHTML = seconds < 10 ? "0" + seconds : seconds;
+        countdownDisplay.children[4].innerHTML = seconds < 10 ? "0" + seconds : seconds;
         if (seconds === 59 && minutes !== 0) {
             minutes = --minutes < 0 && hours !== 0 ? 59 : minutes;
-            countdownDisplay.children[2].innerHTML = minutes < 10 ? "0" + minutes : minutes;
+            countdownDisplay.children[3].innerHTML = minutes < 10 ? "0" + minutes : minutes;
         }
         if (minutes === 59 && hours !== 0) {
             hours = --hours < 0 && days !== 0 ? 23 : hours;
-            countdownDisplay.children[1].innerHTML = hours < 10 ? "0" + hours : hours;
+            countdownDisplay.children[2].innerHTML = hours < 10 ? "0" + hours : hours;
         }
         if (hours === 23 && days !== 0) {
             days = --days;
-            countdownDisplay.children[0].innerHTML = days < 10 ? "0" + days : days;
+            countdownDisplay.children[1].innerHTML = days < 10 ? "0" + days : days;
         }
     }
 }
@@ -47,6 +49,7 @@ submitBtn.addEventListener('click', ev => {
     ev.preventDefault();
     let date = dateInput.value;
     let time = timeInput.value;
+    let name = (nameInput.value).toUpperCase();
 
     let targetDate = Date.parse(date+"T"+time);
     let start = Date.now();
@@ -57,10 +60,11 @@ submitBtn.addEventListener('click', ev => {
     minutes = Math.floor(((remain % 86400) % 3600) / 60);  // 1 min = 60 sec
     seconds = Math.floor(((remain % 86400) % 3600) % 60);
 
-    countdownDisplay.children[0].innerHTML = days < 10 ? "0" + days : days;
-    countdownDisplay.children[1].innerHTML = hours < 10 ? "0" + hours : hours;
-    countdownDisplay.children[2].innerHTML = minutes < 10 ? "0" + minutes : minutes;
-    countdownDisplay.children[3].innerHTML = seconds < 10 ? "O" + seconds : seconds;
+    countdownDisplay.children[0].innerHTML = `COUNTDOWN TO ${name}`
+    countdownDisplay.children[1].innerHTML = days < 10 ? "0" + days : days;
+    countdownDisplay.children[2].innerHTML = hours < 10 ? "0" + hours : hours;
+    countdownDisplay.children[3].innerHTML = minutes < 10 ? "0" + minutes : minutes;
+    countdownDisplay.children[4].innerHTML = seconds < 10 ? "O" + seconds : seconds;
 
     countdownIntervalID = setInterval(countdown, 1000);
 
@@ -90,4 +94,9 @@ resetBtn.addEventListener('click', ev => {
 
 // MAIN
 initDisplay()
+
+// TODO warning of incorrect date
+// TODO warning of overflow date ( > 99 days)
+// TODO add an alert when the event is reached
+// TODO responsive design
 
